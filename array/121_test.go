cbd347a4996229d8ec1f47466e8a04ee948a2b80
yeeyao <yeeyao00@outlook.com>
2020-05-13 17:08:11 +0800

    121

--- array/121_test.go
+++ array/121_test.go
@@ -20,6 +20,24 @@ func TestPro(t *testing.T) {
 			t.Errorf("got: %v, want: %v", got, want)
 		}
 	})
+
+	t.Run("121. Best Time to Buy and Sell Stock3", func(t *testing.T) {
+		input := []int{7, 1, 5, 3, 6, 4}
+		want := 5
+		got := solution2(input)
+		if got != want {
+			t.Errorf("got: %v, want: %v", got, want)
+		}
+	})
+
+	t.Run("121. Best Time to Buy and Sell Stock4", func(t *testing.T) {
+		input := []int{7, 6, 4, 3, 1}
+		want := 0
+		got := solution2(input)
+		if got != want {
+			t.Errorf("got: %v, want: %v", got, want)
+		}
+	})
 }
 
 /*
@@ -41,4 +59,22 @@ func solution(input []int) int {
 		}
 	}
 	return maxBenefit
+}
+
+/*
+	每天只有两个变量 T[i][1][0] 以及 T[i][1][1]
+	T[i][1][0] = max(T[i-1][1][0], T[i-1][1][1] + prices[i])
+	T[i][1][1] = max(T[i-1][1][1], T[i-1][0][0] - prices[i])
+*/
+func solution2(prices []int) int {
+	ti10, ti11 := 0, -99999
+	for _, p := range prices {
+		if ti10 < ti11+p {
+			ti10 = ti11 + p
+		}
+		if ti11 < -p {
+			ti11 = -p
+		}
+	}
+	return ti10
 }
