ac6f62c14ecf9f56ee6d8c2091bb88175d9c6af2
yeeyaohuang <yeeyao00@outlook.com>
2021-11-02 23:05:31 +0800

    491 dfs 枚举方法，待理解

--- array/491_test.go
+++ array/491_test.go
@@ -1,6 +1,7 @@
 package array
 
 import (
+	"math"
 	"testing"
 )
 
@@ -24,30 +25,48 @@ nums 可能含有重复的元素，两个相同的整型可以认为是特殊的
 子函数
 	从第一个开始到最后一个
 	先将当前的加到临时结果中，递归处理，然后删除，继续遍历
+[ref](https://leetcode-cn.com/problems/increasing-subsequences/solution/di-zeng-zi-xu-lie-by-leetcode-solution)
+
+方法 1 是直接二进制枚举 + 哈希 对序列中每个数字，有两种状态，被选中或者不被选中。因此，长度为 n 有 2^n 种序列，然后使用哈希算法去重
+方法 2 递归枚举 + 剪枝，递归方法代替二进制枚举。
+	递归枚举子序列的通用模板，即用一个临时数组 temp 来保存当前选出的子序列，使用 cur 来表示当前位置的下标，在 dfs(cur, nums) 开始之前，[0,cur−1] 这个区间内的所有元素都已经被考虑过，
+	而 [cur,n] 这个区间内的元素还未被考虑。在执行 dfs(cur, nums) 时，我们考虑 cur 这个位置选或者不选，如果选择当前元素，那么把当前元素加入到 temp 中，
+	然后递归下一个位置，在递归结束后，应当把 temp 的最后一个元素删除进行回溯；如果不选当前的元素，直接递归下一个位置。
+	限制条件来枚举出合法且不重复
+		合法则在选择的时候，当前元素大于上一个元素才能选择这个元素
+		没有重复，只有当前元素不等于上一个选择元素的时候，才考虑不选择当前元素，直接递归后面的元素。有两个相同的元素，有四种情况
+			前者被选择，后者被选择
+			前者被选择，后者不被选择
+			前者不被选择，后者被选择
+			前者不被选择，后者不被选择
+	二三种情况是等价的，限制之后，保留了 3 舍弃了 2 可以去重
 */
+var (
+	temp []int
+	ans  [][]int
+)
+
 func solution(nums []int) [][]int {
-	nLen := len(nums)
-	res := make([][]int, 0)
-	genSub(nums, 0, nLen, []int{}, &res)
-	return res
+	ans = [][]int{}
+	dfs(0, math.MinInt32, nums)
+	return ans
 }
 
-func genSub(nums []int, begin, end int, temp []int, res *[][]int) {
-	// 只要临时数组元素数量大于 1 就可以保存到结果数组
-	if len(temp) > 1 {
-		*res = append(*res, temp)
-	}
-	tempLen := len(temp)
-	tempLast := 0
-	if tempLen > 0 {
-		tempLast = temp[tempLen-1]
-	}
-	for i := begin; i < end; i++ {
-		// 临时数组为空或者当前元素大于临时数组的最后一个元素才能添加进去
-		if tempLen == 0 || nums[i] >= tempLast {
-			temp = append(temp, nums[i])
-			genSub(nums, i+1, end, temp, res)
-			temp = temp[tempLen:]
+func dfs(cur, last int, nums []int) {
+	if cur == len(nums) {
+		if len(temp) >= 2 {
+			t := make([]int, len(temp))
+			copy(t, temp)
+			ans = append(ans, t)
 		}
+		return
+	}
+	if nums[cur] >= last {
+		temp = append(temp, nums[cur])
+		dfs(cur+1, nums[cur], nums)
+		temp = temp[:len(temp)-1]
+	}
+	if nums[cur] != last {
+		dfs(cur+1, last, nums)
 	}
 }
