b37c406dd2dcbbe4ccba716f1622f72462d0d02a
yeeyaohuang <yeeyao00@outlook.com>
2022-10-08 22:35:15 +0800

    链表：148 重新完成，添加注释

--- link_list/148_test.go
+++ link_list/148_test.go
@@ -1,6 +1,7 @@
 package link_list
 
 /*
+
 148. Sort List 类似 234
 Sort a linked list in O(n log n) time using constant space complexity.
 链表版本的快排？归并排序
@@ -13,41 +14,50 @@ Sort a linked list in O(n log n) time using constant space complexity.
 将 slow.Next = nil left, right 等于递归调用 head 以及 slow.Next
 初始化两个新节点，其中一个用来进行归并处理
 最后返回未使用的节点
+
+O(nlogn) 的时间复杂度和 O(1) 的空间复杂度。其中最适合链表的排序算法是归并排序。
+
+自顶向下的方法，找到链表中点：
+ - 使用快慢指针，一个走一步，一个走两步，快指针达到结尾的时候，慢指针的位置是链表的中点
+ - 对两个子链表进行排序
+ - 将两个子链表进行归并
 */
+
 func sortList(head *ListNode) *ListNode {
+	// fast 本身可能是 nil
 	if head == nil || head.Next == nil {
 		return head
 	}
-	// 找到中间节点 slow 这里因为 fast 在前面，所以只需要判断 fast
+	// 这里死循环了 fast, slow := head, head
+	// 先找到中间节点，这里 fast 指向下一个
 	fast, slow := head.Next, head
 	for fast != nil && fast.Next != nil {
-		fast = fast.Next.Next
 		slow = slow.Next
+		fast = fast.Next.Next
 	}
-	// 如果是奇数节点，slow 就是中间节点，否则是中间的前一个
-	// 这里递归调用 head 和 slow.Next 将后者设置为 nil 进行截断
+	// 这里需要从中间断开才能实现两端分别排序
 	temp := slow.Next
+	// head.Next 已经不会是 nil 然后 fast 的判断保证了 slow.Next 非 nil
 	slow.Next = nil
-	// 因为最后需要返回，所以这里需要递归的结果
-	left := sortList(head)
-	right := sortList(temp)
-	// 因为这里初始化的是空节点，所以元素加到空节点的 next 中
-	newHead := &ListNode{}
-	res := newHead
-	for left != nil && right != nil {
-		if left.Val < right.Val {
-			newHead.Next = left
-			left = left.Next
+	leftListHead := sortList(head)
+	rightListHead := sortList(temp)
+	// 归并 21
+	retCur := &ListNode{}
+	retHead := retCur
+	for leftListHead != nil && rightListHead != nil {
+		if leftListHead.Val < rightListHead.Val {
+			retCur.Next = leftListHead
+			leftListHead = leftListHead.Next
 		} else {
-			newHead.Next = right
-			right = right.Next
+			retCur.Next = rightListHead
+			rightListHead = rightListHead.Next
 		}
-		newHead = newHead.Next
+		retCur = retCur.Next
 	}
-	if left != nil {
-		newHead.Next = left
+	if leftListHead != nil {
+		retCur.Next = leftListHead
 	} else {
-		newHead.Next = right
+		retCur.Next = rightListHead
 	}
-	return res.Next
+	return retHead.Next
 }
