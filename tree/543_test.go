d783f51ce85d701bbc673b5bdbc48f2e0906b21b
yeeyaohuang <yeeyao00@outlook.com>
2022-11-15 23:40:46 +0800

    树：543 自己的方法

--- tree/543_test.go
+++ tree/543_test.go
@@ -1,53 +1,65 @@
 package tree
 
 /*
-Diameter of Binary Tree
-Given a binary tree, you need to compute the length of the diameter of the tree.
-The diameter of a binary tree is the length of the longest path between any two nodes in a tree.
-This path may or may not pass through the root.
-
-任意两个节点的最长路径，返回边的数量，其实就是求最后一层的节点数量，如果大于 1 就是任意两个节点的
+	这里 节点 的 diameter 其实就是左右子树的高度和
+	同时因为 diameter 可能不经过 root，因此每个节点都需要计算一次，然后更新最大值
+	一开始不确定可以先计算每个节点的高度，然后将其左右子树的高度相加就是当前节点的 diameter，
+	然后发现计算 height 的时候其实已经得到的当前节点的 diameter 因此直接在计算高度的时候顺便判断
 */
-/*
-一条路径的长度为该路径经过的节点数减一，所以求直径（即求路径长度的最大值）等效于求路径经过节点数的最大值减一。
-以该节点为起点的路径经过节点数的最大值即为 L+R+1
-	左儿子向下遍历经过最多的节点数 L 右儿子向下遍历经过最多的节点数 R
-
-其实就是计算当前节点的左右子树深度之和 + 1，最后返回的是每个节点的这个结果的最大值 -1
-	在深度计算过程中更新最大值
-*/
-
-/*
-	题目所求路径是节点的左右子树节点数 + 1 然后 - 1 得到结果
-	先初始化 res = 1
-	递归调用函数后返回 res - 1
-	然后递归计算高度
-		如果当前节点为 nil 返回 0
-		否则，递归调用得到左右节点的高度
-		更新 res
-		返回高度
-*/
-var res int
+var maxDiameter int
 
 func diameterOfBinaryTree(root *TreeNode) int {
-	res = 1
-	helper(root)
-	return res - 1
+	maxDiameter = 0
+	getHeight(root)
+	return maxDiameter
 }
 
-func helper(root *TreeNode) int {
+func getHeight(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
-	left := helper(root.Left)
-	right := helper(root.Right)
-	// 注意这里先计算了最大值，再计算高度
-	if left+right+1 > res {
-		res = left + right + 1
+	leftHeight := getHeight(root.Left)
+	rightHeight := getHeight(root.Right)
+	diameter := leftHeight + rightHeight
+	if diameter > maxDiameter {
+		maxDiameter = diameter
 	}
-	if left > right {
-		return left + 1
+	if leftHeight > rightHeight {
+		return 1 + leftHeight
 	} else {
-		return right + 1
+		return 1 + rightHeight
+	}
+}
+
+// 最开始的版本
+func diameterOfBinaryTreeOld(root *TreeNode) int {
+	maxDiameter = 0
+	diameterOfBinaryTreeHelper(root)
+	return maxDiameter
+}
+
+// 这里需要计算的同时判断是不是最大的
+func diameterOfBinaryTreeHelper(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	leftHeight := getHeight(root.Left)
+	rightHeight := getHeight(root.Right)
+	diameter := leftHeight + rightHeight
+	if diameter > maxDiameter {
+		maxDiameter = diameter
+	}
+}
+
+func getHeightOld(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftHeight := getHeight(root.Left)
+	rightHeight := getHeight(root.Right)
+	if leftHeight > rightHeight {
+		return 1 + leftHeight
+	} else {
+		return 1 + rightHeight
 	}
 }
