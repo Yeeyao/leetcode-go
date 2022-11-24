fe30fa7b6141dff9e9874d9a09e35897675d9073
yeeyaohuang <yeeyao00@outlook.com>
2022-11-24 23:27:28 +0800

    树：面试 04-06 感觉没有看懂题目，待修改

---
+++ tree/mianshi_04_06_test.go
@@ -0,0 +1,34 @@
+package tree
+
+/*
+设计一个算法，找出二叉搜索树中指定节点的“下一个”节点（也即中序后继）。
+如果指定节点没有对应的“下一个”节点，则返回null。
+
+直接中序遍历
+*/
+
+var nextNode *TreeNode
+var isFound bool
+
+func inorderSuccessor(root *TreeNode, p *TreeNode) *TreeNode {
+	nextNode = nil
+	isFound = false
+	inorderSuccessorHelper(root, p)
+	return nextNode
+}
+
+func inorderSuccessorHelper(root *TreeNode, p *TreeNode) {
+	if root == nil {
+		return
+	}
+	inorderSuccessorHelper(root.Left, p)
+	if isFound {
+		nextNode = root
+		isFound = false
+		return
+	}
+	if root == p {
+		isFound = true
+	}
+	inorderSuccessorHelper(root.Right, p)
+}
