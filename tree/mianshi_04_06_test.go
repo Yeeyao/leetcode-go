675f26a702caeca0f1370988ea8ea721165f7ec7
yeeyaohuang <yeeyao00@outlook.com>
2022-12-04 21:52:56 +0800

    树：面试 04-06 利用二叉搜索树性质

--- tree/mianshi_04_06_test.go
+++ tree/mianshi_04_06_test.go
@@ -8,6 +8,38 @@ package tree
 二叉搜索树的中序遍历是单调递增的
 */
 
+/*
+[ref](https://leetcode.cn/problems/successor-lcci/solution/hou-ji-zhe-by-leetcode-solution-6hgc/)
+使用二叉搜索树的性质，这里中序遍历二叉搜索树的性质，则后继节点大于 P 的值，后继节点是 p 之后所有节点的数值最小节点
+如果 p 右子树非空，则 p 后继节点在右子树中，且在其最左的节点。如果 p 右子树为空，则后继节点是 p 的根
+遍历节点，ret 表示 p 后继节点，cur 表示当前遍历的节点。
+	如果 cur > p 则 ret = cur。然后继续向 cur 的左子树遍历
+	如果 cur < p 则继续向 cur 的右子树遍历
+*/
+
+func inorderSuccessor(root *TreeNode, p *TreeNode) *TreeNode {
+	var ret *TreeNode
+	// 如果 p 的右子树非空，则可以直接查找，不需要从 root 遍历了
+	if p.Right != nil {
+		ret = p.Right
+		// 一直找到最左边的子树
+		for ret.Left != nil {
+			ret = ret.Left
+		}
+		return ret
+	}
+	cur := root
+	for cur != nil {
+		if cur.Val > p.Val {
+			ret = cur
+			cur = cur.Left
+		} else {
+			cur = cur.Right
+		}
+	}
+	return ret
+}
+
 var nextNode *TreeNode
 var isFound bool
 
