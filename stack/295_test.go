2c1e4bc71011570bf9c816f8c03d147f67929cfd
yeeyaohuang <yeeyao00@outlook.com>
2023-05-23 23:50:33 +0800

    堆：295 添加题解

--- stack/295_test.go
+++ stack/295_test.go
@@ -1,5 +1,10 @@
 package stack
 
+import (
+	"container/heap"
+	"sort"
+)
+
 /*
 	从数据流中找到中位数。中位数是有序整型列表的中间数值，如果列表元素个数是偶数，则没有中间数值，此时中位数是两个中间数值的平均值
 	需要实现类的方法
@@ -10,27 +15,70 @@ package stack
 	最终的结果就在这两个堆的堆顶中计算。同时，因为都是找的中间的数值，因此这两个堆的元素数量相差值不能大于 1，当这个差值过大，需要将元素在堆之间移动，将较多的移动到较少的
 
 	新增的数字需要怎么判断应该添加到哪个堆里面呢，两个都添加？以及堆之间的元素移动是在哪个时机进行？
+
+	几个问题，首先是元素需要先放到哪个堆。如果放不下第一个堆，是否需要放到第二个堆？两个都放不下，元素应该是需要被丢弃的（表示元素数值是在中间的）
+	什么时候判断堆的大小，放入元素之后吧
+
+	这里给的例子是先放到大顶堆（如果可以放入的话），然后才放到小顶堆。
+	之后比较两个堆的大小，先比较大顶堆元素是否比小顶堆元素数量超过 1，是就将大顶堆堆顶放到小顶堆
+	如果小顶堆元素数量比大顶堆多，则将小顶堆堆顶元素放到大顶堆
+	这里的选择关系到最后返回的结果，如果元素是奇数，则只需要返回大顶堆堆顶，如果偶数，则两个堆数量相等？返回两堆顶的均值
+
+	如果是先放到小顶堆，然后才放到大顶堆，则类似。先放，然后比较堆大小，
+	小顶堆元素是否比大顶堆元素数量超过 1，是就将小顶堆堆顶放到大顶堆
+	这里的选择关系到最后返回的结果，如果元素是奇数，则只需要返回小顶堆堆顶，如果偶数，则两个堆数量相等？返回两堆顶的均值
+	[ref](https://leetcode.cn/problems/find-median-from-data-stream/solution/shu-ju-liu-de-zhong-wei-shu-by-leetcode-ktkst/)
+
+	times: 1
+
 */
 
+// 因为两个都是使用 int slice 因此小顶堆需要将元素取相反数再入堆
 type MedianFinder struct {
-	MinHeap []int
-	MaxHeap []int
+	MinHeap hp
+	MaxHeap hp
 }
 
 func Constructor() MedianFinder {
-	return MedianFinder{
-		MinHeap: make([]int, 0),
-		MaxHeap: make([]int, 0),
-	}
-
+	return MedianFinder{}
 }
 
 func (this *MedianFinder) AddNum(num int) {
-
+	minH, maxH := &this.MinHeap, &this.MaxHeap
+	// 等于也要放入
+	if minH.Len() == 0 || num <= -minH.IntSlice[0] {
+		heap.Push(minH, -num)
+	} else {
+		heap.Push(maxH, num)
+	}
+	// 需要注意符号变化
+	if minH.Len()-maxH.Len() > 1 {
+		heap.Push(maxH, -heap.Pop(minH).(int))
+	} else if maxH.Len() > minH.Len() {
+		heap.Push(minH, -heap.Pop(maxH).(int))
+	}
 }
 
 func (this *MedianFinder) FindMedian() float64 {
+	minH, maxH := this.MinHeap, this.MaxHeap
+	if minH.Len() > maxH.Len() {
+		return float64(-minH.IntSlice[0])
+	}
+	return float64(-minH.IntSlice[0]+maxH.IntSlice[0]) / 2
+}
 
+type hp struct {
+	sort.IntSlice
+}
+
+func (h *hp) Push(v interface{}) {
+	h.IntSlice = append(h.IntSlice, v.(int))
+}
+
+func (h *hp) Pop() interface{} {
+	top := h.IntSlice[len(h.IntSlice)-1]
+	h.IntSlice = h.IntSlice[:len(h.IntSlice)-1]
+	return top
 }
 
 /**
