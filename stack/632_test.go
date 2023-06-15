442be122936a0d20c771098d61ccdf5c0e54dd8d
yeeyaohuang <yeeyao00@outlook.com>
2023-06-15 23:51:58 +0800

    堆：632 添加注释

--- stack/632_test.go
+++ stack/632_test.go
@@ -3,7 +3,8 @@ package stack
 /*
 632. Smallest Range Covering Elements from K Lists
 你有 k 个 非递减排列 的整数列表。找到一个 最小 区间，使得 k 个列表中的每个列表至少有一个数包含在其中。
-
+找交叉？
+这道题本质上就是在 m 个一维数组中各取出一个数字，重新组成新的数组 A，使得新的数组 A 中最大值和最小值的差值（diff）最小。
 */
 
 func smallestRange(nums [][]int) []int {
