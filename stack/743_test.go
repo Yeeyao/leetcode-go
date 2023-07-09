7b2eec91d7a8685c49d22cdc7b2732a46a531d81
yeeyaohuang <yeeyao00@outlook.com>
2023-07-09 21:19:22 +0800

    堆：743 添加题目描述

---
+++ stack/743_test.go
@@ -0,0 +1,9 @@
+package stack
+
+/*
+	给定网络的 n 个节点，标号从 1 到 n 同时给定次数 times 数组，times[i] = (ui, vi, wi) 表示从 ui 到 vi 节点需要的时间是 wi。
+	我们会从给定节点 k 发送一个信号，返回所有的 n 个节点收到信号的所需的最小时间。如果不可能全都收到就返回 -1
+	这里其实是计算消耗时间最长的路径
+*/
+func networkDelayTime(times [][]int, n int, k int) int {
+}
