337714a54fc8f3f73dfbd79da60807df52fad6b8
yeeyaohuang <yeeyao00@outlook.com>
2023-07-11 23:40:13 +0800

    堆：743 dijkstra 效率很低的版本

--- stack/743_test.go
+++ stack/743_test.go
@@ -5,14 +5,48 @@ import "container/heap"
 /*
 	给定网络的 n 个节点，标号从 1 到 n 同时给定次数 times 数组，times[i] = (ui, vi, wi) 表示从 ui 到 vi 节点需要的时间是 wi。
 	我们会从给定节点 k 发送一个信号，返回所有的 n 个节点收到信号的所需的最小时间。如果不可能全都收到就返回 -1
-	这里其实是计算消耗时间最长的路径
+	这里其实是计算消耗时间最长的最短路径
 */
+
 func networkDelayTime(times [][]int, n int, k int) int {
+	// 需要构造 graph
+	graph := make(map[int][]*gn)
+	for _, v := range times {
+		start := v[0]
+		next := v[1]
+		cost := v[2]
+		if _, ok := graph[start]; ok {
+			graph[start] = append(graph[start], &gn{
+				pos:  next,
+				cost: cost,
+			})
+		} else {
+			graph[start] = []*gn{
+				{next, cost},
+			}
+		}
+	}
+
+	maxMinWeight := -1
+	for i := 1; i <= n; i++ {
+		if i == k {
+			continue
+		}
+		weight := dijkstra(graph, k, i)
+		if weight == -1 {
+			return -1
+		}
+		if weight > maxMinWeight {
+			maxMinWeight = weight
+		}
+	}
+	return maxMinWeight
 	// 对应 dijkstra 开始是 k 结束是所有的节点，然后取其中的最大值
 }
 
 // graph[i][j] = c 表示从 i 到 j 需要 cost c
-func dijkstra(graph [][]int, start, end int) int {
+
+func dijkstra(graph map[int][]*gn, start, end int) int {
 	// 先将开始节点放入
 	h := &graphNodes{gnList: []*gn{}}
 	// 从 start 到 start cost 0
@@ -35,55 +69,14 @@ func dijkstra(graph [][]int, start, end int) int {
 		if pos == end {
 			return cost
 		}
-		for i, v := range graph[pos] {
-			if _, ok := visitedMap[i]; ok {
+		for _, next := range graph[pos] {
+			if _, ok := visitedMap[next.pos]; ok {
 				continue
 			}
 			heap.Push(h, &gn{
-				pos:  i,
-				cost: cost + v,
+				pos:  next.pos,
+				cost: cost + next.cost,
 			})
-		}
-	}
-	return -1
-}
-
-func dijkstraBackup(graph [][]int, start, end int) int {
-	// 先将开始节点放入
-	h := &graphNodes{gnList: []*gn{}}
-	// 从 start 到 start cost 0
-	h.Push(&gn{
-		pos:  start,
-		cost: 0,
-	})
-	// 记录已经访问的节点
-	visitedMap := make(map[int]interface{})
-	for h.Len() > 0 {
-		topNodes := heap.Pop(h).(*gn)
-		pos := topNodes.pos
-		cost := topNodes.cost
-		// 已经遍历过就跳过
-		if _, ok := visitedMap[pos]; ok {
-			continue
-		}
-		visitedMap[pos] = struct{}{}
-		// 如果已经遇到最后的节点了，直接返回
-		if pos == end {
-			return cost
-		}
-		for _, v := range graph {
-			// 找到当前节点开始的图
-			if v[0] == pos {
-				// 如果已经遍历过就跳过
-				if _, ok := visitedMap[v[1]]; ok {
-					continue
-				}
-				// 没有遍历过，开始到下一个节点的 cost 等于开始到当前节点的 cost 加上 当前节点到下一个节点的 cost
-				heap.Push(h, &gn{
-					pos:  v[1],
-					cost: cost + v[2],
-				})
-			}
 		}
 	}
 	return -1
