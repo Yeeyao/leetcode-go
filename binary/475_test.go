79e77db9e640c9476892c2312399e395e7994329
yeeyaohuang <yeeyao00@outlook.com>
2023-07-20 23:50:01 +0800

    二分：475 添加题目

---
+++ binary/475_test.go
@@ -0,0 +1,50 @@
+package binary
+
+import "sort"
+
+/*
+冬天来了，你需要设计一个标准的加热器来加热所有的房子，所有的加热器有相同的加热半径。
+给定 houses 以及 heaters 的坐标数组，返回让所有房子都可以被加热的最小半径
+看起来给定的数组都是升序的（非降序）
+
+遍历加热器然后判断当前的加热器需要的半径？
+
+对一个加热器来说，其所需半径等于前后房子所需的半径
+- 前面的半径等于当前的加热器和前一个加热器的距离的一半
+- 后面的半径也类似
+- 上面两个成立的条件是两个加热器 之前存在房子，如果不存在房子则对应的一边的半径可以是 0
+
+需要使用一个全局的半径来进行更新
+
+将房子和加热器的坐标都记录下来，记录下坐标同时记录下坐标下是房子还是加热器或者两个都有
+- 一般的情况是，记录最左边的加热器，等到遇到下一个加热器的时候，此时两个加热器之间有房子就需要更新两者之间的距离的一半为当前需要的半径
+- 两个加热器之间是否有房子这个要怎么快速判断呢
+- 房子和加热器的关系最左边和最右边的单独判断一下就好了
+
+
+*/
+func findRadius(houses []int, heaters []int) int {
+	sort.Ints(houses)
+	sort.Ints(heaters)
+	var radius int
+	// 先处理最左边的情况 最左边需要覆盖
+	leftMostRadius := heaters[0] - houses[0]
+	if leftMostRadius > radius {
+		radius = leftMostRadius
+	}
+	// 中间部分，两个加热器到
+	var prevPos int
+	for _, v := range heaters {
+		// 找到
+		if prevPos != 0 {
+		}
+		prevPos = v
+	}
+
+	// 最右边处理
+	rightMostRadius := houses[len(houses)-1] - heaters[len(heaters)-1]
+	if rightMostRadius > radius {
+		radius = rightMostRadius
+	}
+
+}
